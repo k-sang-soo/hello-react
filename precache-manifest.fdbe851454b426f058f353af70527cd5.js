64599f80d83868adac0a968cac279910de220086
k-sang-soo <wsckm1@gmail.com>
2020-07-09 12:28:58 +0900

    Updates

---
+++ precache-manifest.fdbe851454b426f058f353af70527cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc736690873e4d3424e4027b13f2637",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "22b93ac28bf9a56b5037",
+    "url": "/hello-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "879139f4c7cefead6089",
+    "url": "/hello-react/static/js/2.d49b7fe6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-react/static/js/2.d49b7fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b93ac28bf9a56b5037",
+    "url": "/hello-react/static/js/main.4cd5e3ff.chunk.js"
+  },
+  {
+    "revision": "61b7d1b2978b87885f93",
+    "url": "/hello-react/static/js/runtime-main.1dbbaa17.js"
+  }
+]);
